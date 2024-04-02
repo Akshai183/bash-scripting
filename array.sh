05fb0f648aa32276f168463860f5f69ce91762a4
Akshai <akshay05pakhanati@gmail.com>
2024-04-02 08:00:35 +0530

    List - using netcat command to check port details

--- array.sh
+++ array.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #declare an array or list of values
 SERVERS=('10.0.0.1' '10.37.32.164' '19.24.2.4' '9.13.4.2')
-for SERVER in ${SERVERS[@]}
-do
-timeout 2 telnet $SERVER 22
+for SERVER in ${SERVERS[@]}; do
+    echo "Pinging Server: $SERVER"
+    necat -z -n -v $SERVER 22 80 2>&1 | grep succeeded > /tmp/reachable.txt
 done
