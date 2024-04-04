96b752800551b7bd1f7c084bd2abe6cbf5722f10
Akshai <akshay05pakhanati@gmail.com>
2024-04-04 11:45:08 +0530

    User exists or not create new user - Slack Notifications

--- class_7-CreateUser.sh
+++ class_7-CreateUser.sh
@@ -10,13 +10,13 @@ if [ $# -gt 0 ]; then
         echo "Lets Create User ${USERNAME}."
         sudo useradd -m $USERNAME --shell /bin/bash -d /home/${USERNAME}
         sudo usermod -a -G root $USERNAME
-        SPEC=$(echo '!@#$%^&*()_' | fols -w1 | shuf | tail -1)
+        SPEC=$(echo '!@#$%^&*()_' | fold -w1 | shuf | tail -1)
         PASSWORD="India@${RANDOM}${SPEC}" 
-        echo "Password for ${USERNAME} is ${PASSWORD}" | sudo chpasswd
+        echo "Password for ${USERNAME}:${PASSWORD}" | sudo chpasswd
         passwd -e ${USERNAME}
         echo "The temporary credentials are ${USERNAME} and ${PASSWORD"
-        curl -X POST ${SLACK_WEB} -sL -H 'Content-type: application/json' --data "{"text": \"Username is: ${USERNAME}.\"}" >>/dev/null
-        curl -X POST ${SLACK_WEB} -sL -H 'Content-type: application/json' --data "{"text": \"Temporary-Password is: ${PASSWORD} Reset the password immediately.\"}" >>/dev/null
+        curl -X POST ${SLACK_WEB} -sL -H 'Content-type: application/json' --data "{"text": \"Username is: ${USERNAME}\"}" >>/dev/null
+        curl -X POST ${SLACK_WEB} -sL -H 'Content-type: application/json' --data "{"text": \"Temporary-Password is: ${PASSWORD} Reset the password immediately\"}" >>/dev/null
     fi 
 else
     echo "You have not given $# arguments please provide atleast 1 args."
