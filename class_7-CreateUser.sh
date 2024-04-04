64773af61dc5a4c2c32237b8d45a5071d5db5489
Akshai <akshay05pakhanati@gmail.com>
2024-04-04 10:40:02 +0530

    User exists or not create new user

--- class_7-CreateUser.sh
+++ class_7-CreateUser.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 USERNAME=$1
-EXISTING_USER=$(cat /etc/passwd | grep -i -w ${USERNAME}| cut -d ':' -f 1)
+EXISTING_USER=$(cat /etc/passwd | grep -i ${USERNAME}| cut -d ':' -f 1)
 if [ "${USERNAME}" == "${EXISTING_USER}" ]; then
     echo "User ${USERNAME} already exists."
 else
