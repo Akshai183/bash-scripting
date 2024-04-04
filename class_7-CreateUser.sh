c0de8149a9c70e0b3be03d9ea9d7011105cc761d
Akshai <akshay05pakhanati@gmail.com>
2024-04-04 12:09:16 +0530

    User exists or not create new user - Slack Notifications

--- class_7-CreateUser.sh
+++ class_7-CreateUser.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 #set -x
 SLACK_WEB='https://hooks.slack.com/services/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'
-USERNAME=$1
+#USERNAME=$1
 if [ $# -gt 0 ]; then
-    if id "$USERNAME" &>/dev/null; then
-        echo "User ${USERNAME} already exists."
-    else
-        echo "Let's create user ${USERNAME}."
-        sudo useradd -m "$USERNAME" --shell /bin/bash -d /home/"${USERNAME}"
-        sudo usermod -a -G root "$USERNAME"
-        SPEC=$(echo '!@#$%^&*()_' | fold -w1 | shuf | tail -1)
-        PASSWORD="India@${RANDOM}${SPEC}" 
-        echo "${USERNAME}:${PASSWORD}" | sudo chpasswd
-        passwd -e "${USERNAME}"
-        echo "The temporary credentials are ${USERNAME} and ${PASSWORD}"
-        curl -X POST "${SLACK_WEB}" -sL -H 'Content-type: application/json' --data "{\"text\": \"Username is: ${USERNAME}\"}" >>/dev/null
-        curl -X POST "${SLACK_WEB}" -sL -H 'Content-type: application/json' --data "{\"text\": \"Temporary-Password is: ${PASSWORD} Reset the password immediately\"}" >>/dev/null
-    fi 
+    for USERNAME in $@; do
+        EXISTING_USER=$(cat /etc/passwd | grep -i -w ${USERNAME} | cut -d ':' -f 1)
+        if [ "${USERNAME}" = "${EXISTING_USER}" ]; then
+            echo "The User ${USERNAME} Already Exists. Try A Diffrent Username."
+        else
+            echo "Lets Create User ${USERNAME}."
+            sudo useradd -m $USERNAME --shell /bin/bash -d /home/${USERNAME}
+            sudo usermod -aG sudo ${USERNAME}
+            echo '${USERNAME} ALL=(ALL) NOPASSWD: ALL' >>/etc/sudoers
+            SPEC=$(echo '!@#$%^&*()_' | fold -w1 | shuf | head -1)
+            PASSWORD="India@${RANDOM}${SPEC}"
+            echo "${USERNAME}:${PASSWORD}" | sudo chpasswd
+            passwd -e ${USERNAME}
+            echo "The Temporary Credentails are ${USERNAME} and ${PASSWORD}"
+            curl -X POST ${SLACK_WEB} -sL -H 'Content-type: application/json' --data "{"text": \"Username is: ${USERNAME}\"}" >>/dev/null
+            curl -X POST ${SLACK_WEB} -sL -H 'Content-type: application/json' --data "{"text": \"Temporary Password Is: ${PASSWORD}  Reset This Password Immediatly.\"}" >>/dev/null
+
+        fi
+    done
 else
-    echo "You have not given $# arguments. Please provide at least 1 arg."
+    echo "You Have Given $# Arguments. Please Provide Atleast One Arg."
 fi
