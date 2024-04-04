a06bdfa874c681c50eeb8257fa189a126bfaedeb
Akshai <akshay05pakhanati@gmail.com>
2024-04-04 10:51:31 +0530

    User exists or not create new user

--- class_7-CreateUser.sh
+++ class_7-CreateUser.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 USERNAME=$1
-EXISTING_USER=$(cat /etc/passwd | grep -i ${USERNAME} | cut -d ':' -f 1)
-if [ "${USERNAME}" == "${EXISTING_USER}" ]; then
+if [ $# -gt 0 ]; then
+   EXISTING_USER=$(cat /etc/passwd | grep -i -w ${USERNAME} | cut -d ':' -f 1)
+   if [ "${USERNAME}" == "${EXISTING_USER}" ]; then
     echo "User ${USERNAME} already exists."
+    else
+        echo "Lets Create User ${USERNAME}."
+    fi 
 else
-    echo "Lets Create User ${USERNAME}."
+echo "You have not given $# arguments please provide atleast 1 args."
 fi
