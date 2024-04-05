23f61bcd920adc3f22a6959fb115d1e51d752d4d
Akshai <akshay05pakhanati@gmail.com>
2024-04-05 09:17:40 +0530

    functions -  Delete unattached volumes Ec2

--- class-9_functions.sh
+++ class-9_functions.sh
@@ -3,7 +3,7 @@ delete_vols() {
     vols=$(aws ec2 describe-volumes --region us-east-1 | jq '.Volumes[].VolumeId' | tr -d '"')
     for vol in $vols; do
         status=$(aws ec2 describe-volumes --volume-ids $vol | jq ".Volumes[].Attachments[].State" | tr -d '"')
-        if $status == "attached"; then
+        if [[ "${STATUS}" == 'attached' ]]; then
             echo "Volume $vol is attached to an instance. Skipping deletion."
         else
             echo "Deleting volume $vol as its not in use"
