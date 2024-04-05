6e757376ca74677f18445c6abed5ff6775973775
Akshai <akshay05pakhanati@gmail.com>
2024-04-05 09:36:29 +0530

    functions -  Delete unattached volumes Ec2

--- class-9_functions.sh
+++ class-9_functions.sh
@@ -2,22 +2,20 @@
 set -euo pipefail
 delete_unattached_vols() {
     echo " Lets check volume status per $region"
-    vols=$(aws ec2 describe-volumes --region us-east-1 | jq -r '.Volumes[].VolumeId')
+    vols=$(aws ec2 describe-volumes --region $region | jq -r '.Volumes[].VolumeId')
     for vol in $vols; do
         status=$(aws ec2 describe-volumes --volume-ids "$vol" | jq -r ".Volumes[].Attachments[].State")
         if [[ "$status" == 'attached' ]]; then
             echo "Volume $vol is attached to an instance. Skipping deletion."
         else
             echo "Deleting volume $vol as it's not in use"
-            aws ec2 delete-volume --volume-id "$vol"
+            aws ec2 delete-volume --volume-id $vol --region $region
         fi
     done
-    for region in $(aws ec2 describe-regions | jq '.Regions[].RegionName' | tr -d '"'); do
-        delete_unattached_vols $region
-    done
 }
-
-delete_vols
+for region in $(aws ec2 describe-regions | jq '.Regions[].RegionName' | tr -d '"'); do
+    delete_unattached_vols $region
+done
 
 # delete unattached volumes
 # * * * * * /bin/bash /root/bash/class-9_functions.sh
