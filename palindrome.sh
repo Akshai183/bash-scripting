c7abe731db2ad55a4b0747e4ffa27dfb549f9051
Akshai <akshay05pakhanati@gmail.com>
2024-04-05 12:37:38 +0530

    ShellScripting - Palindrome - checking code

--- palindrome.sh
+++ palindrome.sh
@@ -3,11 +3,11 @@ WORD='MALAYALAM'
 WORD_LENGTH=${#WORD}
 REVERSE=''
 LENGTH=$(expr $WORD_LENGTH - 1)
-for ((i=$LENGTH; i>=0; i--)); do
+for ((i = $LENGTH; i >= 0; i--)); do
     REVERSE=${REVERSE}${WORD[@]:$i:1}
 done
 echo $REVERSE
-if [[ "${REVERSE} = "${WORD}" ]]; then
+if [[ "${REVERSE}" = "${WORD}" ]]; then
     echo "Palindrome"
 else
     echo "Not Palindrome"
