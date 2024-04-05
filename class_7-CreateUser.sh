74e51c019dcefc2819d3cd1f0827bf0bbc6c656f
Akshai <akshay05pakhanati@gmail.com>
2024-04-05 07:42:55 +0530

    User exists or not create new user - Regular Expressions

--- class_7-CreateUser.sh
+++ class_7-CreateUser.sh
@@ -4,22 +4,26 @@ SLACK_WEB='https://hooks.slack.com/services/T06S04VS797/B06STM8TW5A/rq5hXRFfUuWW
 #USERNAME=$1
 if [ $# -gt 0 ]; then
     for USERNAME in $@; do
-        EXISTING_USER=$(cat /etc/passwd | grep -i -w ${USERNAME} | cut -d ':' -f 1)
-        if [ "${USERNAME}" = "${EXISTING_USER}" ]; then
-            echo "The User ${USERNAME} Already Exists. Try A Diffrent Username."
-        else
-            echo "Lets Create User ${USERNAME}."
-            sudo useradd -m $USERNAME --shell /bin/bash -d /home/${USERNAME}
-            sudo usermod -aG sudo ${USERNAME}
-            echo '${USERNAME} ALL=(ALL) NOPASSWD: ALL' >>/etc/sudoers
-            SPEC=$(echo '!@#$%^&*()_' | fold -w1 | shuf | head -1)
-            PASSWORD="India@${RANDOM}${SPEC}"
-            echo "${USERNAME}:${PASSWORD}" | sudo chpasswd
-            passwd -e ${USERNAME}
-            echo "The Temporary Credentails are ${USERNAME} and ${PASSWORD}"
-            curl -X POST ${SLACK_WEB} -sL -H 'Content-type: application/json' --data "{"text": \"Username is: ${USERNAME}\"}" >>/dev/null
-            curl -X POST ${SLACK_WEB} -sL -H 'Content-type: application/json' --data "{"text": \"Temporary Password Is: ${PASSWORD}  Reset This Password Immediatly.\"}" >>/dev/null
+        if [[ $USERNAME =~ ^[a-z][a-z][a-z][0-9][0-9][0-9]$ ]]; then
+            EXISTING_USER=$(cat /etc/passwd | grep -i -w ${USERNAME} | cut -d ':' -f 1)
+            if [ "${USERNAME}" = "${EXISTING_USER}" ]; then
+                echo "The User ${USERNAME} Already Exists. Try A Diffrent Username."
+            else
+                echo "Lets Create User ${USERNAME}."
+                sudo useradd -m $USERNAME --shell /bin/bash -d /home/${USERNAME}
+                sudo usermod -aG sudo ${USERNAME}
+                echo '${USERNAME} ALL=(ALL) NOPASSWD: ALL' >>/etc/sudoers
+                SPEC=$(echo '!@#$%^&*()_' | fold -w1 | shuf | head -1)
+                PASSWORD="India@${RANDOM}${SPEC}"
+                echo "${USERNAME}:${PASSWORD}" | sudo chpasswd
+                passwd -e ${USERNAME}
+                echo "The Temporary Credentails are ${USERNAME} and ${PASSWORD}"
+                curl -X POST ${SLACK_WEB} -sL -H 'Content-type: application/json' --data "{"text": \"Username is: ${USERNAME}\"}" >>/dev/null
+                curl -X POST ${SLACK_WEB} -sL -H 'Content-type: application/json' --data "{"text": \"Temporary Password Is: ${PASSWORD}  Reset This Password Immediatly.\"}" >>/dev/null
 
+            fi
+        else
+            echo "The Username ${USERNAME} is Invalid. Please Provide A Valid Username."
         fi
     done
 else
