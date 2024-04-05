9923f489954a7f0f414ff2218b536f516c9eb1ce
Akshai <akshay05pakhanati@gmail.com>
2024-04-05 09:32:31 +0530

    functions -  Delete unattached volumes Ec2

--- class-9_functions.sh
+++ class-9_functions.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -euo pipefail
-
-delete_vols() {
+delete_unattached_vols() {
     vols=$(aws ec2 describe-volumes --region us-east-1 | jq -r '.Volumes[].VolumeId')
     for vol in $vols; do
         status=$(aws ec2 describe-volumes --volume-ids "$vol" | jq -r ".Volumes[].Attachments[].State")
@@ -12,8 +11,12 @@ delete_vols() {
             aws ec2 delete-volume --volume-id "$vol"
         fi
     done
+    for region in $(aws ec2 describe-regions | jq '.Regions[].RegionName' | tr -d '"'); do
+        delete_unattached_vols $region
+    done
 }
 
 delete_vols
 
 # delete unattached volumes
+# * * * * * /bin/bash /root/bash/class-9_functions.sh
