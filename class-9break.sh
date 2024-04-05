82ae9778ed3d6ef981c27bc3077ed26ce43eb834
Akshai <akshay05pakhanati@gmail.com>
2024-04-05 10:58:23 +0530

    functions - continue * Avaliability Zones AWS

--- class-9break.sh
+++ class-9break.sh
@@ -18,7 +18,7 @@ Get_VPC() {
             echo "##########################"
             echo "# Breaking at $region #"
             echo "#########################"
-            continue
+            break
         fi
     done
 }
